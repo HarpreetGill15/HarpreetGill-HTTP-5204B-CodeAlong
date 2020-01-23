720758fea191cbe4fbf2e0e95c7c45f6b16c9302
Harpreet Gill <harpreet_gill_@outlook.com>
2020-01-23 13:00:22 -0500

    first commit

---
+++ Models/Owner.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+//Install  entity framework 6 on Tools > Manage Nuget Packages > Microsoft Entity Framework (ver 6.4)
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace PetGrooming.Models
+{
+    public class Owner
+    {
+        /*
+            An owner is someone who owns one or more pets
+            Some things that describe an owner:
+                - First Name
+                - Last Name
+                - Address
+                - Phone Number (work)
+                - Phone Number (home)
+
+            An owner must reference a list of pets
+            
+        */
+        [Key]
+        public int OwnerId { get; set; }
+        public string OwnerFirstName { get; set; }
+        public string OwnerLastName { get; set; }
+        public string OwnerEmail { get; set; }
+        public string OwnerPrimaryNumber { get; set; }
+        public string OwnerSecondaryNumber { get; set; }
+        public string OwnerAddress { get; set; }
+    }
+}
